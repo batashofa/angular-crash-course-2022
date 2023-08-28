ba05687a6acdb449dfbefb02f76f72c59cb66f16
batashofa <Vegavetta1424>
2023-08-28 13:45:33 +0300

    feat: added error service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ProductComponent} from "./components/product/product.component";
 import {HttpClientModule} from "@angular/common/http";
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductComponent
+    ProductComponent,
+    GlobalErrorComponent
   ],
   imports: [
     BrowserModule,
